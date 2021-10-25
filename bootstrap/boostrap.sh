ba4f5ac4ea07dc0f591043369927854d4e381121
tianyuan129 <tianyuan@cs.ucla.edu>
2021-10-25 12:36:44 -0700

    add example trust schema

---
+++ bootstrap/boostrap.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# install customized ndncert
+# mkdir dep && cd mkdir dep
+# git clone https://github.com/tianyuan129/ndncert.git
+# cd ndncert && git checkout v0.3
+# ./waf configure
+# sudo ./waf install && cd ..
+
+help()
+{
+   echo "Usage: $0 -c ssl_cert -p ssl_prv -d name_component"
+   echo -c "\t SSL Certificate"
+   echo -p "\t Private key for the supplied SSL Certificate"
+   exit 1 # Exit script after printing help
+}
+
+python3 bootstrap/json-writter.py
+sudo cp bootstrap/ndncert-client.conf /usr/local/etc/ndncert/client.conf
+
+while getopts c:p: flag
+do
+    case "${flag}" in
+        c) ssl_cert=${OPTARG};;
+        p) ssl_prv=${OPTARG};;
+        ?) help;;
+    esac
+done
+
+if [ -c "$ssl_cert" ] || [ -p "$ssl_prv" ]
+then
+   help
+   exit 1
+fi
+
+echo "SSL Certificate path: $ssl_cert"
+echo "Private key for SSL Certificate: $ssl_prv"
+python3 bootstrap/auto.py -c $ssl_cert -p $ssl_prv
---
+++ bootstrap/json-writter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+import argparse
+import os
+import json
+from typing import Dict
+
+ca_cert = ""
+with open("bootstrap/hydra-anchor.cert", "r", encoding='utf-8') as ca_encoded:
+    ca_cert = ca_encoded.read()
+
+with open("bootstrap/ndncert-client.conf", "r+", encoding='utf-8') as client_conf:
+    json_data = client_conf.read()
+    json_config = json.loads(json_data)
+    json_config['ca-list'][0]['certificate'] = ca_cert
+    client_conf.seek(0)
+    client_conf.write(json.dumps(json_config, indent=2))
+    client_conf.truncate()
