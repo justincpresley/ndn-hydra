34df5c7354b8fff66f0e74b0a952d9841b4b9eb8
Nick Mynatt <62191506+nick-mynatt@users.noreply.github.com>
2022-09-08 20:14:16 -0500

    Favor Calculator implementation
    Only distance based favor calculations for now.

--- ndn_hydra/repo/modules/favor_calculator.py
+++ ndn_hydra/repo/modules/favor_calculator.py
@@ -9,5 +9,33 @@
 #  @Pip-Library:   https://pypi.org/project/ndn-hydra
 # -------------------------------------------------------------
 
+import logging
+import numpy as np
+
 class FavorCalculator:
-    pass
+    """
+    A class for abstracting favor calculations between two nodes.
+    """
+    def distance_based_favor(self, latA: float, lonA: float, latB: float, lonB: float) -> float:
+        """
+        Generate favor for a node based on distance.
+        """
+        def sphere_dist(latA: float, lonA: float, latB: float, lonB: float) -> float:
+            R = 6378.137 # earth radius
+            # convert to radians
+            latA = latA * (np.pi/180)
+            lonA = lonA * (np.pi/180)
+            latB = latB * (np.pi/180)
+            lonB = lonB * (np.pi/180)
+            # calculate distance
+            DeltaS = np.arccos(
+                np.sin(latA) * np.sin(latB) + \
+                np.cos(latA) * np.cos(latB) * np.cos(lonA-lonB)
+            )
+            distance = R * DeltaS
+            return distance
+
+        favor = sphere_dist(latA, lonA, latB, lonB)
+        return round(favor, 2)
+
+    
